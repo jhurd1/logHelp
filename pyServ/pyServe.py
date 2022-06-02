06b9010a3f7b3cf3066c9b0e4d1113d2c038b38b
jhurd1 <jamiehurd3@gmail.com>
2022-06-01 20:06:52 -0600

    flask improved

--- pyServ/pyServe.py
+++ pyServ/pyServe.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run()
 
 @app.route('/user', methods=['POST'])
 def user():
-    data = request.get_json()
-    print(data)
-    return data
+    fpath = request.form.get("fpath", "")
+    searchStrings = request.form.get("searchStrings", "")
+    anonymize = request.form.get("anonymize", "")
+    return jsonify(fpath=fpath, searchStrings=searchStrings, anonymize=anonymize)
