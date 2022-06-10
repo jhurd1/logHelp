0899bd0b58f5ca2f4bd50d5c41f1f28b387c34c2
jhurd1 <jamiehurd3@gmail.com>
2022-06-09 18:04:34 -0600

    import statement resolved react render error

--- loghelp/src/index.js
+++ loghelp/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 //import ServiceUser from "./ServiceUser";
