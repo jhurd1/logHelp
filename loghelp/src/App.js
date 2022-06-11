ded710f290fd65aa8a1b327e80475ca2dbc643dd
jhurd1 <jamiehurd3@gmail.com>
2022-06-11 16:07:01 -0600

    simplifying turns to debugging const var construction for sendData

--- loghelp/src/App.js
+++ loghelp/src/App.js
@@ -1,30 +1,46 @@
+import axios from 'axios';
 import React, {useEffect, useState} from 'react';
-import TakeInput from './TakeInput';
-
-// Reflects App.js in example code; see line 130 in Notes.txt.
+import './SCSS/index.css';
+//import TakeInput from './TakeInput';
 
 function App() {
-    // Where is setData defined? It's not in TakeInput.js right now
-    const [data, setData] = useState([]);
+    const [fpath, setPath] = useState([]);
+    const [searchStrings, setStrings] = useState([]);
+    const sendData = (props) => {
+        return (
+        props.fpath,
+        props.searchStrings
+    );};
 
-    //Self: I know you want to push stuff to backend
-    // but let's keep testing this first, then try that later.
-    useEffect(()=>{
-        fetch('http://localhost:5000/data', {
-            'methods':'GET',
-            headers:{
-                'Content-Type':'application/json'
-            }
-    })
-    .then(response => response.json())
-    .then(response => setData(response))
-    .catch(error => console.log(error))
-},[])
+    sendData = (e) => {
+        e.preventDefault();
+        axios.post("http://localhost:3000", {
+            fpath: document.getElementById("fpath").value,
+            searchStrings: document.getElementById("searchStrings").value
+        }).then((res)=>{
+            console.log(res.data);
+        });
+    }
 
     return (
-        <div className="TakeInput">
-            <TakeInput insertData={insertData}/>
-        </div>
+        <div>
+                <form className={"grid_layout"} onSubmit={this.sendData}>
+                    <p className={"heading"}>Welcome to your web-app log helper tool!</p>
+                    <p className={"appDescription"}>LogHelper is designed to assist in the sharing and anonymization of log data using
+                        an automated process for replacing names, IP addresses, Mac addresses, and other
+                        sensitive data for the exchange of said data during troubleshooting and case
+                        management.</p>
+                    <div className={"center"}>
+                        <label>Enter your path and search strings here:<br/><br/>
+                            <input type="text" value={fpath}  placeholder="file path"/>
+                            <input type="text" value={searchStrings} placeholder="search strings" />
+                            <button type="submit">Submit</button>
+                        </label>
+                        <br/>
+                    </div>
+                </form>
+
+            </div>
     );
 }
 
--- pyServ/pyServe.py
+++ pyServ/pyServe.py
@@ -1,25 +1,13 @@
 from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy
-from OtherData import OtherData, data_schema
 
-#equivalent to the routes.py file in the example project
-
-db = sqlalchemy
 app = Flask(__name__)
 
-@app.route('/data', methods=['POST'])
+@app.route('/', methods=['POST'])
 def add_data():
     fpath = request.json['fpath']
     searchStrings = request.json['searchStrings']
-
-    data = OtherData (
-        fpath = fpath,
-        searchStrings = searchStrings
-    )
-
-    db.session.add(data)
-    db.session.commit()
+    return {'fpath':'Test success for ' + fpath,
+    'searchStrings': 'Test success for ' + searchStrings}
 
 if __name__ == '__main__':
     app.run(debug=True)
