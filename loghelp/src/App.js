91c6a9792c68769212f4486d2501c0793167829e
jhurd1 <jamiehurd3@gmail.com>
2022-06-14 20:39:04 -0600

    the value prop error resolved by removing value property from input jsx tags

--- loghelp/src/App.js
+++ loghelp/src/App.js
@@ -4,8 +4,8 @@ import './SCSS/index.css';
 //import TakeInput from './TakeInput';
 
 function App() {
-    const [fpath, setPath] = useState([]);
-    const [searchStrings] = useState();
+    const [fpath, setPath] = useState("");
+    const [searchStrings, setStrings] = useState("");
     /*let sendData = (props) => {
         return (
         props.fpath,
@@ -23,7 +23,7 @@ function App() {
     }
 
     return (
-                <form className={"grid_layout"} onSubmit={sendData}>
+                <form className={"grid_layout"} onChange={e => {setPath(e.target.value); setStrings(e.target.value);}} onSubmit={sendData}>
                     <p className={"heading"}>Welcome to your web-app log helper tool!</p>
                     <p className={"appDescription"}>LogHelper is designed to assist in the sharing and anonymization of log data using
                         an automated process for replacing names, IP addresses, Mac addresses, and other
@@ -31,9 +31,9 @@ function App() {
                         management.</p>
                     <div className={"center"}>
                         <label>Enter your path and search strings here:<br/><br/>
-                            <input type="text" value={fpath}  placeholder="file path"/>
-                            <input type="text" value={searchStrings} placeholder="search strings" />
-                            <button type="submit" onClick={()=>setPath("")}>Submit</button>
+                            <input type="text" placeholder="file path"/>
+                            <input type="text" placeholder="search strings" />
+                            <button type="submit" onChange={e => {setPath(e.target.value); setStrings(e.target.value);}} onClick={()=>{setPath(""); setStrings("");}} onSubmit={sendData}>Submit</button>
                         </label>
                         <br/>
                     </div>
