1a064a5e82aba38da908a09e549a0cc8d8b38651
jhurd1 <jamiehurd3@gmail.com>
2022-06-16 20:50:56 -0600

    rolled back to workable state

--- loghelp/src/App.js
+++ loghelp/src/App.js
@@ -1,73 +1,13 @@
-import axios from 'axios';
-import React, {useState} from 'react';
-import './SCSS/index.css';
-//import TakeInput from './TakeInput';
+import React from 'react';
+import TakeInput from './TakeInput';
 
+// The technical PARENT COMPONENT of all other components in a React app.
 function App() {
-    const {fpath, searchStrings} = this.props;
-    constructor(props)
-     super(props);
-    
-     this.state = {fpath:"", searchStrings:""};
-     this.handleSubmit = this.handleSubmit.bind(this);
-    
-    
-
-    componentDidUpdate(prevProps, prevState)
-
-    componentDidUpdate(prevProps)
-    {
-        if(this.props.fpath !== prevPropsw.fpath, this.props.searchStrings !== prevProps.searchStrings)
-        {
-            this.fetchData(this.props.fpath, this.props.searchStrings);
-        }
-    }
-
-    //const [search, setSearch] = useState(addSearch)
-
-    let sendData = (e) => {
-        e.preventDefault();
-        axios.post("http://localhost:3000", {
-            fpath: document.getElementById("fpath").value,
-            searchStrings: document.getElementById("searchStrings").value
-        }).then((res)=>{
-            console.log(res.data);
-        });
-    }
-
-    // original is commented out in takeinput for second version control option
-    const handleChange = (e) => {
-        setSearch(prev => ({
-            ...prev,
-            [e.target.fpath]: e.target.value,
-            [e.target.searchStrings]: e.target.value
-        }))
-        e.preventDefault()
-        // test console.log for debugging
-        console.log(e);
-        }
-
     return (
-        <div>
-                <form className={"grid_layout"}>
-                    <p className={"heading"}>Welcome to your web-app log helper tool!</p>
-                    <p className={"appDescription"}>LogHelper is designed to assist in the sharing and anonymization of log data using
-                        an automated process for replacing names, IP addresses, Mac addresses, and other
-                        sensitive data for the exchange of said data during troubleshooting and case
-                        management.</p>
-                    <div className={"center"}>
-                        <label>Enter your path and search strings here:<br/><br/>
-                            <input type="text" placeholder="file path" value={setSearch.fpath} onChange={handleChange}/>
-                            <input type="text" placeholder="search strings" value={setSearch.searchStrings} onChange={handleChange}/>
-                            <button type="submit" onChange={handleChange} onClick={setSearch} onSubmit={sendData}>Submit</button>
-                        </label>
-                        <br/>
-                    </div>
-                </form>
-                </div>
-
-    );
+     <div>
+         <TakeInput/>
+     </div>
+  );
 }
-
 
 export default App;
--- loghelp/src/TakeInput.js
+++ loghelp/src/TakeInput.js
@@ -1,56 +1,13 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './SCSS/index.css';
-import ServiceUser from "./ServiceUser";
-
-// Reflects Form.js in the example code
-function TakeInput(){
-const Form = (props) => {
-    const [fpath, setPath] = useState('')
-    const [searchStrings, setStrings] = useState('')
-    const insertData = () => {
-        ServiceUser.InsertData({fpath, searchStrings})
-        .then((response) => props.insertData(response))
-        .catch(error => console.log('error', error))
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        insertData()
-        setPath('')
-        setStrings('')
-    }
-
-    return (
-        <div>
-                <form className={"grid_layout"} onSubmit={this.handleSubmit}>
-                    <p className={"heading"}>Welcome to your web-app log helper tool!</p>
-                    <p className={"appDescription"}>LogHelper is designed to assist in the sharing and anonymization of log data using
-                        an automated process for replacing names, IP addresses, Mac addresses, and other
-                        sensitive data for the exchange of said data during troubleshooting and case
-                        management.</p>
-                    <div className={"center"}>
-                        <label>Enter your path and search strings here:<br/><br/>
-                            <input type="text" value={fpath} onChange={(e)=>setPath(e.target.value)} placeholder="file path"/>
-                            <input type="text" value={searchStrings} onChange={(e)=>setStrings(e.target.value)} placeholder="search strings" />
-                            <button onClick={this.handleSubmit.bind(this)}>{'Search'}</button>
-                        </label>
-                        <br/>
-                    </div>
-                </form>
-
-            </div>
-    )
- }
-}
+import ServiceUser from './ServiceUser';
 
 // shouldn't need react hook since classes are supposed to do the same thing
-/*export class TakeInput extends React.Component{
-
+export class TakeInput extends React.Component{
     // constructor properties or "props" permits passing default values to callers
     constructor(props)
     {
         super(props);
-
         this.state = {
             user: {
                 fpath: props.fpath,
@@ -64,22 +21,17 @@ const Form = (props) => {
         searchStrings: "",
         anonymize:""
     };
-
     handleChange=(e)=>{
-
         //current value of the user
         const user = this.state.user;
-
         //extract value of input embodied in 'target'
         const modifiedPath = e.target.fpath;
         const modifiedStrings = e.target.searchStrings;
         const modifiedAnonymize = e.target.anonymize;
-
         //update user
         user.fpath = modifiedPath;
         user.searchStrings = modifiedStrings;
         user.anonymize = modifiedAnonymize;
-
         // test console.log for debugging
         console.log(e);
         this.setState({
@@ -88,7 +40,6 @@ const Form = (props) => {
             anonymize: e.target.anonymize
         })
     };
-
     handleSubmit = (event) => {
         event.preventDefault();
         
@@ -99,7 +50,6 @@ const Form = (props) => {
             console.log(error)
         })
     };
-
     render()
     {
         return (
@@ -120,14 +70,12 @@ const Form = (props) => {
                         <p>Anonymize results?</p>
                         <input type="radio" value={this.state.user.anonymize} id="yes"
                                onChange={this.handleChange.bind(this)} name="anonymize?"/> <label htmlFor="yes">Yes</label>
-
                         <input type="radio" value={this.state.user.anonymize} id="no"
                                onChange={this.handleChange.bind(this)} name="anonymize?"/> <label htmlFor="No">No</label>
                     </div>
                 </form>
-
             </div>
         );
     }
-}*/
+}
 export default TakeInput;
